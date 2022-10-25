e6962c11e6db6150b8cd0674a57a4f21a6f7a259
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-10-25 22:29:03 +0530

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptor/wrap-response.interceptor';
@@ -19,6 +20,16 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new WrapResponseInterceptor());
+
+  /* OpenAPI Specification */
+  const options = new DocumentBuilder()
+    .setTitle('Coffee API')
+    .setDescription("Coffee api's endpoints.")
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
